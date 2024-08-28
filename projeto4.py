146057232bdfc41be91a28575fef479a771c78c4
Arthur de Sousa <arthur.smota@sempreceub.com>
2024-08-28 10:15:06 -0300

    projeto4.py

--- projeto4.py
+++ projeto4.py
@@ -7,5 +7,5 @@ elif compra > venda:
     print(f"Teve prejuízo {diferenca}")
 else:
     print("Os valores são iguais")
-print(f"Valor da compra: {compra:.21200f}")
+print(f"Valor da compra: {compra:.2f}")
 print(f"Valor da venda: {venda:.2f}")
